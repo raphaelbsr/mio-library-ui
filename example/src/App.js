5a93ccad7532912695e66aaf3ca1695454dc1473
raphaelbsr <raphael@datac.com.br>
2020-02-12 13:27:34 -0300

    LabelValue Component

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
-import { ActionDialog, Table } from 'mio-library-ui';
+import { ActionDialog, LabelValue } from 'mio-library-ui';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <ActionDialog isOpen={true}>Testando</ActionDialog>
+        {/* <ActionDialog isOpen={true}>Testando</ActionDialog> */}
         {/* <Table /> */}
+        <LabelValue label="Label" value="Value" />
       </div>
     );
   }
