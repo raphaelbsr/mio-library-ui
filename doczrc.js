e20ebb669a87d53dfe21a5c8bb63cb4307783d37
raphaelbsr <raphael@datac.com.br>
2020-10-29 12:16:46 -0300

    docz finals

--- doczrc.js
+++ doczrc.js
@@ -1,3 +1,11 @@
 export default {
   typescript: true,
+  menu: [
+    'Mio Library UI',
+    'Readme',
+    {
+      name: 'Componentes'
+    }
+    // { name: '🔗 natalia', href: 'https://natalia.com/' route : ''},
+  ],
 }
