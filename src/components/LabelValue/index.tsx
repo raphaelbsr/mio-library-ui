906fd7d1693e89f0f0b4a789d9193d9c4fae20e9
Raphael Barbosa <raphael@datac.com.br>
2021-07-26 10:58:57 -0300

    label value adjustments

--- src/components/LabelValue/index.tsx
+++ src/components/LabelValue/index.tsx
@@ -1,45 +1,69 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
-import { Box, Typography, Theme } from '@material-ui/core';
+import { Box, Typography, Theme, Divider, makeStyles } from '@material-ui/core';
 
 interface LabelValueProps {
   label: string,
   value?: string,
   valueComponent?: React.ReactElement
+  direction?: 'horizontal' | 'vertical'
+  alignItems?: string
+  justifyContent?: string
+  separator?: string,
+  showSeparator?: boolean,
+  showDivider?: boolean,
 }
 
-const LabelValue: React.FC<LabelValueProps> = ({ label, value, valueComponent }) => {
-  const useStyles = makeStyles<Theme>(theme => ({
-    box: {
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-between'
-    },
-    label: {
-      paddingRight: theme.spacing(1)
-    },
-    value: {
-    }
-  }));
+const useStyles = makeStyles<Theme, LabelValueProps>(theme => ({
+  box: {
+    display: 'flex',
+    flexDirection: (props) =>
+      props.direction === "horizontal" ? "row" : "column",
+    alignItems: (props) => props.alignItems,
+    justifyContent: (props) => props.justifyContent,
+  },
+  label: {
+    fontWeight: "bolder",
+    fontSize: "9pt",
+  },
+  value: {
+    marginLeft: theme.spacing(0.5),
+    fontSize: "10pt",
+  }
+}));
 
-  const classes = useStyles();
+const LabelValue: React.FC<LabelValueProps> = (props) => {
+
+  const { label, value, separator, showSeparator, showDivider } = props
+
+  const classes = useStyles(props);
   return (
     <Box className={classes.box}>
-      <Typography
-        className={classes.label}
-        variant="subtitle1"
-        component="span">
-        {label}:
+      <Typography className={classes.label} variant="body2">
+        {label}
+        {showSeparator && separator}
       </Typography>
-      <Typography
+      {showDivider && <Divider style={{ flex: 1 }} />}
+      <Typography className={classes.value} variant="body2">
+        {value}
+      </Typography>
+      {/* <Typography
         className={classes.value}
         variant="subtitle1"
         component="span"
       >
         {valueComponent ? valueComponent : value}
-      </Typography>
+      </Typography> */}
     </Box>
   );
 };
+
+LabelValue.defaultProps = {
+  separator: ":",
+  direction: "horizontal",
+  alignItems: "flex-end",
+  justifyContent: "flex-start",
+  showSeparator: true,
+  showDivider: false,
+}
 
 export default LabelValue;
